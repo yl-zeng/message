821df16501028cb71e4c88aa771a2a0263e69884
yl-zeng <ylinzeng69@gmail.com>
2017-08-23 17:56:10 -0400

    set up router

--- routes/app.js
+++ routes/app.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+  User.findOne({}, function(err, doc) {
+    if(err) {
+      return res.send("Error!");
+    }
+    res.render('node', {email: doc.email});
+  });
+});
+
+router.post('/message', function(req, res, next) {
+  var email = req.body.email;
+  var user = new User({
+    firstName: 'Qing',
+    lastName: 'Zeng',
+    password: '123456',
+    email: email
+  });
+  user.save();
+  res.redirect('/');
 });
 
 module.exports = router;
