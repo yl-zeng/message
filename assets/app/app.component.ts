ea6523c61afa3b81ebeabc504f7dfc089d1018b5
yl-zeng <ylinzeng69@gmail.com>
2017-08-23 23:06:21 -0400

    create template for my-app component

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
 })
 export class AppComponent {
-    
+  message = {
+    content: "A message",
+    author: "Qing"
+  }
 }
--- routes/app.js
+++ routes/app.js
@@ -3,24 +3,7 @@ var router = express.Router();
 var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-  User.findOne({}, function(err, doc) {
-    if(err) {
-      return res.send("Error!");
-    }
-    res.render('node', {email: doc.email});
-  });
-});
-
-router.post('/message', function(req, res, next) {
-  var email = req.body.email;
-  var user = new User({
-    firstName: 'Qing',
-    lastName: 'Zeng',
-    password: '123456',
-    email: email
-  });
-  user.save();
-  res.redirect('/');
+  res.render('index');
 });
 
 module.exports = router;
