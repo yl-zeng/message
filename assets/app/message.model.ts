362a1415e4251b1f2a1257107ba8b388a33e729e
yl-zeng <ylinzeng69@gmail.com>
2017-08-23 23:35:07 -0400

    set up User model for angular

--- assets/app/message.model.ts
+++ assets/app/message.model.ts
@@ -1,8 +1,8 @@
 export class Message {
   content: string;
   username: string;
-  messageId: string;
-  userId: string;
+  messageId?: string;
+  userId?: string;
 
   constructor(content: string, username: string, messageId: string, userId: string) {
     this.content = content;
