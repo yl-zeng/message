74e500420f63b537e34a14645d1d9d92dfadfb50
yl-zeng <ylinzeng69@gmail.com>
2017-08-25 17:29:07 -0400

    Create MessageInputComponent

---
+++ assets/app/messages/message-input.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'app-message-input',
+    templateUrl: './message-input.component.html'
+})
+export class MessageInputComponent {
+
+}
