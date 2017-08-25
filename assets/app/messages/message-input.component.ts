db7da366ce603b765113235b9a4933357c21baa9
yl-zeng <ylinzeng69@gmail.com>
2017-08-25 18:01:31 -0400

    Create Message Service and Inject into MessageInputComponent

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,11 +1,19 @@
 import {Component} from "@angular/core";
+import {MessageService} from "./message.service";
+import {Message} from "./message.model";
 
 @Component({
     selector: 'app-message-input',
-    templateUrl: './message-input.component.html'
+    templateUrl: './message-input.component.html',
+    providers: [MessageService]
 })
 export class MessageInputComponent {
+    constructor(private messageService: MessageService) {
+
+    }
+
     onSave(value: string) {
-        console.log(value);
+        const message = new Message(value, 'Zeng');
+        this.messageService.addMessage(message);
     }
 }
