afa6abf858266af2d1b8785b160ca031a64f4b2e
yl-zeng <ylinzeng69@gmail.com>
2017-08-25 21:34:41 -0400

    Create subRouter and Wire up

--- assets/app/auth/authentication.component.ts
+++ assets/app/auth/authentication.component.ts
@@ -1,7 +1,20 @@
 import {Component} from "@angular/core";
 @Component({
     selector: 'app-authentication',
-    template: `<h1>Authentication</h1>`
+    template: `
+        <header class="row spacing">
+            <nav class="col-md-8 col-md-offset-2">
+                <ul class="nav nav-tabs">
+                    <li routerLinkActive="active" [routerLink]="['signup']"><a>Signup</a></li>
+                    <li routerLinkActive="active" [routerLink]="['signin']"><a>Signin</a></li>
+                    <li routerLinkActive="active" [routerLink]="['logout']"><a>Logout</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="row spacing">
+            <router-outlet></router-outlet>
+        </div>
+    `
 })
 export class AuthenticationComponent {
 
