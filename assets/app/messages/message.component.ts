c37243aa549f8a86063131ed9932d92ce1406e47
yl-zeng <ylinzeng69@gmail.com>
2017-08-25 21:36:05 -0400

    Front end clean up

--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 import {Message} from "./message.model";
+import {MessageService} from "./message.service";
 
 
 @Component({
@@ -25,9 +26,15 @@ export class MessageComponent {
   @Input('inputMessage') message: Message;
   @Output() editClicked = new EventEmitter<string>();
 
+  constructor(private messageService: MessageService) {}
+
   color = "blue";
 
   onEdit() {
       this.editClicked.emit('A new value');
+  }
+
+  onDelete() {
+    this.messageService.deleteMessage(this.message);
   }
 }
