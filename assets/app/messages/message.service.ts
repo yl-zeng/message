5ab0d912e835bcbe791faf580006d85bf33b3718
yl-zeng <ylinzeng69@gmail.com>
2017-08-26 22:23:29 -0400

    Set up Http Request for getMessage in front end

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -21,7 +21,16 @@ export class MessageService {
     }
 
     getMessages() {
-        return this.messages;
+        return this.http.get('http://localhost:3000/message')
+            .map((response: Response) => {
+                const messages = response.json().obj;
+                var transformedMessages: Message[] = [];
+                for(var message of messages) {
+                    transformedMessages.push(new Message(message.content, message.id, 'Dummy', null));
+                }
+                return transformedMessages;
+            })
+            .catch((error: Response) => Observable.throw(error.json()));
     }
 
     deleteMessage(message: Message) {
