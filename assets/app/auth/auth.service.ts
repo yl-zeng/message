27cb7bfd706742ea85efa6fc22ef15e436d5fcc9
yl-zeng <ylinzeng69@gmail.com>
2017-08-28 23:14:51 -0400

    Create Auth service and Signup Method

---
+++ assets/app/auth/auth.service.ts
@@ -0,0 +1,18 @@
+import {User} from "./user.model";
+import {Injectable} from "@angular/core";
+import {Http, Response, Headers} from "@angular/http";
+import 'rxjs/Rx';
+import {Observable} from "rxjs/Observable";
+
+@Injectable()
+export class AuthService {
+    constructor(private http:Http) {}
+
+    signup(user: User) {
+        const body = JSON.stringify(user);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.post('http://localhost:3000/user', body, {headers: headers})
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
+    }
+}
