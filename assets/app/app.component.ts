d94011c6dd0bcbee1012fe7612adc24da033c35f
yl-zeng <ylinzeng69@gmail.com>
2017-08-28 23:22:39 -0400

    Wire up user signup method with backend

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {Message} from "./messages/message.model";
 import {MessageService} from "./messages/message.service";
+import {AuthService} from "./auth/auth.service";
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
-    providers: [MessageService]
+    providers: [MessageService, AuthService]
 })
 export class AppComponent {
 
--- assets/app/auth/signup.component.ts
+++ assets/app/auth/signup.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from "@angular/core";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "./auth.service";
+import {User} from "./user.model";
 @Component({
     selector: 'app-signup',
     templateUrl: './signup.component.html'
@@ -7,8 +9,22 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 export class SignupComponent implements OnInit{
     myForm: FormGroup;
 
+    constructor(private authService: AuthService) {}
+
     onSubmit() {
         console.log(this.myForm);
+        const user = new User(
+            this.myForm.value.email,
+            this.myForm.value.password,
+            this.myForm.value.firstName,
+            this.myForm.value.lastName
+        );
+        this.authService.signup(user)
+            .subscribe(
+                data => console.log(data),
+                error => console.error(error)
+            );
+        this.myForm.reset();
     }
 
     ngOnInit() {
