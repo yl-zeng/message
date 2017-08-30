a5f0f8db24761f47fec9145f1593e77c3704815b
yl-zeng <ylinzeng69@gmail.com>
2017-08-29 23:08:04 -0400

    Send token in angular http request

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -15,7 +15,9 @@ export class MessageService {
     addMessage(message: Message) {
         const body = JSON.stringify(message);
         const headers = new Headers({'Content-Type': 'application/json'});
-        return this.http.post('http://localhost:3000/message',body, {headers: headers})
+        const token = localStorage.getItem('token') ?
+            '?token=' + localStorage.getItem('token') : '';
+        return this.http.post('http://localhost:3000/message' + token,body, {headers: headers})
             .map((response: Response) => {
                 const result = response.json();
                 const message = new Message(result.obj.content, 'Dummy', result.obj._id, null);
@@ -47,15 +49,19 @@ export class MessageService {
     updateMessage(message: Message) {
         const body = JSON.stringify(message);
         const headers = new Headers({'Content-Type': 'application/json'});
-        return this.http.patch('http://localhost:3000/message/' + message.messageId, body, {headers: headers})
+        const token = localStorage.getItem('token') ?
+            '?token=' + localStorage.getItem('token') : '';
+        return this.http.patch('http://localhost:3000/message/' + message.messageId + token, body, {headers: headers})
             .map((response: Response) => response.json())
             .catch((error: Response) => Observable.throw(error.json()));
     }
 
     deleteMessage(message: Message) {
         this.messages.splice(this.messages.indexOf(message),1);
+        const token = localStorage.getItem('token') ?
+            '?token=' + localStorage.getItem('token') : '';
 
-        return this.http.delete('http://localhost:3000/message/' + message.messageId)
+        return this.http.delete('http://localhost:3000/message/' + message.messageId + token)
             .map((response: Response) => response.json())
             .catch((error: Response) => Observable.throw(error.json()));
     }
