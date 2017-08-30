dd61fa4ca395db5b2d925a452dfb503fbf406814
yl-zeng <ylinzeng69@gmail.com>
2017-08-30 18:31:10 -0400

    Create ErrorService

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import {Message} from "./messages/message.model";
 import {MessageService} from "./messages/message.service";
 import {AuthService} from "./auth/auth.service";
+import {ErrorService} from "./errors/error.service";
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
-    providers: [MessageService, AuthService]
+    providers: [MessageService, AuthService, ErrorService]
 })
 export class AppComponent {
 
